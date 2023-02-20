f0c137d355e278f521dca688c09ba79f79391c5e
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2023-02-21 01:28:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { fontStyles } from "./styles";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 style={fontStyles}>Some styled title!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,6 @@
+export const fontStyles = {
+  color: "#ff0000",
+  fontSize: "1.2rem",
+  fontWeight: "bold",
+  fontStyle: "italic"
+};
